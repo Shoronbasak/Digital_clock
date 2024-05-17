77562ce5d0ce252d13cbdfd03a5c481d54041c7c
Shoronbasak <81005965+Shoronbasak@users.noreply.github.com>
2024-05-17 21:19:21 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,24 @@
+let hrs = document.getElementById("hrs")
+let min = document.getElementById("min")
+let sec = document.getElementById("sec")
+let date = document.getElementById("date")
+
+setInterval(() => {
+    
+    let currentTime = new Date();
+    
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    date.innerHTML = currentTime.toDateString()
+    
+}, 1000);
+
+
+
+
+
+
+
+
+
